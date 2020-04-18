d6390092eaf8b7aee731df35426723a74f6349e7
TEA <kmd36rk@gmail.com>
2020-04-18 13:41:19 +0700

    Icrease latency

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -883,17 +883,17 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 	/* get pm-qos-active-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-active-latency",
 		&device->pwrctrl.pm_qos_active_latency))
-		device->pwrctrl.pm_qos_active_latency = 301;
+		device->pwrctrl.pm_qos_active_latency = 401;
 
 	/* get pm-qos-cpu-mask-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,l2pc-cpu-mask-latency",
 		&device->pwrctrl.pm_qos_cpu_mask_latency))
-		device->pwrctrl.pm_qos_cpu_mask_latency = 301;
+		device->pwrctrl.pm_qos_cpu_mask_latency = 401;
 
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
-		device->pwrctrl.pm_qos_wakeup_latency = 81;
+		device->pwrctrl.pm_qos_wakeup_latency = 1;
 
 	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
 		timeout = 64;
